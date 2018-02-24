8626647f89be12fbb135a0129dff75c032977b7c
David Bouchare <david.bouchare@datadoghq.com>
2018-02-24 18:37:00 +0100

    Merge Financials into Stocks

--- src/Net/Stocks.hs
+++ src/Net/Stocks.hs
@@ -2,8 +2,10 @@
 
 module Net.Stocks
        ( Stock(..)
+       , Financials(..)
        , Company
        , getStocksResp
+       , getFinancialsResp
        ) where
   
 import Control.Monad
@@ -27,6 +29,24 @@ instance FromJSON Stock where
         <*> v .: "changePercent"
     parseJSON _          = mzero
 
+-- | Financials data
+data Financials = 
+    Financials { reportDate       :: String  -- ^ The report's date
+               , grossProfit      :: Int     -- ^ Gross Profit
+               , costOfRevenue    :: Int     -- ^ Cost of revenue
+               , cashFlow         :: Int     -- ^ Cash flow
+               } deriving (Show)
+
+-- https://stackoverflow.com/questions/16547783/parse-array-in-nested-json-with-aeson
+instance FromJSON Financials where
+    parseJSON (Object v) = do
+        financialsV <- head <$> v.: "financials"
+        Financials <$> financialsV .: "reportDate"
+                   <*> financialsV .: "grossProfit"
+                   <*> financialsV .: "costOfRevenue"
+                   <*> financialsV .: "cashFlow"
+    parseJSON _          = mzero
+
 type Company = String
 
 -- builds the URL: /stock/{symbol}/quote
@@ -34,8 +54,19 @@ stocksQuery :: Company -> String
 stocksQuery company =
     "https://api.iextrading.com/1.0/stock/" ++ company ++ "/quote"
 
+-- builds the URL: /stock/{symbol}/financials
+financialsQuery :: Company -> String
+financialsQuery company =
+    "https://api.iextrading.com/1.0/stock/" ++ company ++ "/financials"
+
 -- get JSON data 
 getStocksResp :: String -> IO (Maybe Stock)
 getStocksResp company = do
     obj <- simpleHttp (stocksQuery company)
+    return $ decode obj
+
+-- get JSON data 
+getFinancialsResp :: String -> IO (Maybe Financials)
+getFinancialsResp company = do
+    obj <- simpleHttp (financialsQuery company)
     return $ decode obj
