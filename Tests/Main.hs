bf5e009067530ccb8b133504ac2804a45ecadbfb
Kristian Sallberg <kristian@purestyle.se>
2018-05-18 20:56:58 +0200

    Implement the batch API partially

--- Tests/Main.hs
+++ Tests/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Test.HUnit
 import Net.Stocks
+import Data.Maybe
 
 main :: IO Counts
 main = runTestTT tests
@@ -19,10 +20,14 @@ tests = TestList [TestLabel "" testChart,
                   TestLabel "" testOHLC,
                   TestLabel "" testPeers,
                   TestLabel "" testPrevious,
-                  -- TestLabel "" testPrice, -- FIXME: how to test?
+                  TestLabel "" testPrice,
                   TestLabel "" testQuote,
                   TestLabel "" testSplit,
-                  TestLabel "" testVolumeByVenue]
+                  TestLabel "" testVolumeByVenue,
+                  TestLabel "" testTypeQuery1,
+                  TestLabel "" testBatch,
+                  TestLabel "" testMultiBatch1,
+                  TestLabel "" testMultiBatch2]
 
 testChart = TestCase (do result <- getChart "aapl"
                          assertBool "desc" (result /= Nothing))
@@ -50,9 +55,40 @@ testPeers = TestCase
         (L8.pack "[\"MSFT\",\"NOK\",\"IBM\",\"HPQ\",\"GOOGL\",\"BB\",\"XLK\"]"))
 testPrevious = TestCase (do result <- getPrevious "aapl"
                             assertBool "desc" (result /= Nothing))
+testPrice = TestCase (do result <- getPrice "aapl"
+                         assertBool "" (result /= Nothing))
 testQuote = TestCase (do result <- getQuote "aapl"
                          assertBool "desc" (result /= Nothing))
 testSplit = TestCase (do result <- getSplit "aapl"
                          assertBool "desc" (result /= Nothing))
 testVolumeByVenue = TestCase (do result <- getVolumeByVenue "aapl"
                                  assertBool "desc" (result /= Nothing))
+testTypeQuery1 = TestCase (assertEqual "" "types=news,ohlc"
+                           (typeQuery [NewsQuery, OHLCQuery]))
+
+-- FIXME: when record names can be uniquely defined, we need to
+--        have more specific tests, i.e. not just /= Nothing
+
+-- the following batch requests asks for stats, news, company:
+-- getBatchCompany "aapl" [StatsQuery, NewsQuery, CompanyQuery]
+-- testBatch = TestCase
+--   (do result <- getBatchCompany "aapl" [StatsQuery, NewsQuery, CompanyQuery]
+--       assertEqual "" (pickCompany result) "Apple Inc.")
+
+-- pickCompanyName :: Maybe Batch -> String
+-- pickCompanyName (Just (Batch {stats = st})) = pickCompanyName' st
+
+-- pickCompanyName' :: Maybe Company -> String
+-- pickCompanyName' (Just (Stats {companyName = cname})) = fromJust cname
+
+testBatch = TestCase
+  (do result <- getBatchCompany "aapl" [StatsQuery, NewsQuery, CompanyQuery]
+      assertBool "" (result /= Nothing))
+
+testMultiBatch1 = TestCase
+  (do result <- getBatch ["fb"] [OHLCQuery]
+      assertBool "" (result /= Nothing))
+
+testMultiBatch2 = TestCase
+  (do result <- getBatch ["dps", "fb"] [OHLCQuery, CompanyQuery]
+      assertBool "" (result /= Nothing))
